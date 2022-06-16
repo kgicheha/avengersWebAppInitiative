7c9a03c8d4a28344a117e6924fb5da394b26dcfb
Chris Hendricks <crshenx@gmail.com>
2022-06-16 16:22:17 -0700

    did this work?

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const memeApiUrl = "https://api.imgflip.com/get_memes";
+let memeData;
+const memeOftheDay = document.getElementById("meme-of-the-day-img");
 
+fetch(memeApiUrl)
+  .then((res) => res.json())
+  .then((data) => renderMeme(data));
+
+function renderMeme(meme) {
+  // memeData = meme;
+  let memesArr = meme.data.memes;
+  let rando = Math.floor(Math.random() * memesArr.length);
+  console.log(rando);
+  memeOftheDay.src = memesArr[rando].url;
+  memeOftheDay.style.height = 316;
+  memeOftheDay.style.width = 420;
+  console.log(meme);
+}
